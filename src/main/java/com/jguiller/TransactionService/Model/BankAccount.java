7cb797325110be0cc9a8529eb262a7afcc016c2d
JhojanGuille <guille.balvin@gmail.com>
2020-08-03 18:13:53 -0500

    Lombok add

--- src/main/java/com/jguiller/TransactionService/Model/BankAccount.java
+++ src/main/java/com/jguiller/TransactionService/Model/BankAccount.java
@@ -7,7 +7,12 @@ import javax.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "BankAccounts")
+@Data @NoArgsConstructor @AllArgsConstructor
 public class BankAccount {
 	
 	@Id
@@ -24,57 +29,5 @@ public class BankAccount {
 	private Double montoCuenta;
 	
 	private String fechaCreacionCuenta;
-	
-	public BankAccount() {}
-
-	public int getIdCuenta() {
-		return idCuenta;
-	}
-
-	public void setIdCuenta(int idCuenta) {
-		this.idCuenta = idCuenta;
-	}
-
-	public int getIdCliente() {
-		return idCliente;
-	}
-
-	public void setIdCliente(int idCliente) {
-		this.idCliente = idCliente;
-	}
-
-	public int getIdProducto() {
-		return idProducto;
-	}
-
-	public void setIdProducto(int idProducto) {
-		this.idProducto = idProducto;
-	}
-
-	public Double getMontoCuenta() {
-		return montoCuenta;
-	}
-
-	public void setMontoCuenta(Double montoCuenta) {
-		this.montoCuenta = montoCuenta;
-	}
-
-	public String getFechaCreacionCuenta() {
-		return fechaCreacionCuenta;
-	}
-
-	public void setFechaCreacionCuenta(String fechaCreacionCuenta) {
-		this.fechaCreacionCuenta = fechaCreacionCuenta;
-	}
-
-	public BankAccount(@NotNull int idCuenta, @NotNull int idCliente, @NotNull int idProducto, Double montoCuenta,
-			String fechaCreacionCuenta) {
-		super();
-		this.idCuenta = idCuenta;
-		this.idCliente = idCliente;
-		this.idProducto = idProducto;
-		this.montoCuenta = montoCuenta;
-		this.fechaCreacionCuenta = fechaCreacionCuenta;
-	}
 	
 }
--- src/main/java/com/jguiller/TransactionService/Model/Transaction.java
+++ src/main/java/com/jguiller/TransactionService/Model/Transaction.java
@@ -7,7 +7,12 @@ import javax.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "Transactions")
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Transaction {
 	
 	@Id
@@ -19,58 +24,5 @@ public class Transaction {
 	private String tipoTransaccion;
 	private Double montoTransaccion;
 	private String fechaTransaccion;
-	
-	public Transaction() {}
-
-	public int getIdTransaccion() {
-		return idTransaccion;
-	}
-
-	public void setIdTransaccion(int idTransaccion) {
-		this.idTransaccion = idTransaccion;
-	}
-
-	public int getIdCuenta() {
-		return idCuenta;
-	}
-
-	public void setIdCuenta(int idCuenta) {
-		this.idCuenta = idCuenta;
-	}
-
-	public String getTipoTransaccion() {
-		return tipoTransaccion;
-	}
-	
-	public void setTipoTransaccion(String tipoTransaccion) {
-		this.tipoTransaccion = tipoTransaccion;
-	}
-	
-	public Double getMontoTransaccion() {
-		return montoTransaccion;
-	}
-
-	public void setMontoTransaccion(Double montoTransaccion) {
-		this.montoTransaccion = montoTransaccion;
-	}
-
-	public String getFechaTransaccion() {
-		return fechaTransaccion;
-	}
-
-	public void setFechaTransaccion(String fechaTransaccion) {
-		this.fechaTransaccion = fechaTransaccion;
-	}
-
-	public Transaction(@NotNull int idTransaccion, int idCuenta, String tipoTransaccion, Double montoTransaccion, String fechaTransaccion) {
-		super();
-		this.idTransaccion = idTransaccion;
-		this.idCuenta = idCuenta;
-		this.tipoTransaccion = tipoTransaccion;
-		this.montoTransaccion = montoTransaccion;
-		this.fechaTransaccion = fechaTransaccion;
-	}
-	
-	
 	
 }
